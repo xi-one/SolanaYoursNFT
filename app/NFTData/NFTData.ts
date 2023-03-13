d6663b1e24ad8dc1847693af22c1c31a013483cd
xi-one <ssly1133@naver.com>
2023-03-13 10:16:17 +0900

    first commit

---
+++ app/NFTData/NFTData.ts
@@ -0,0 +1,12 @@
+export class NFTData {
+    title: string;
+    symbol: string;
+    uri: string;
+
+    constructor(name: string) {
+        this.title = `NFT ${name}`;
+        this.symbol = `${name}`;
+        //this.uri = `https://raw.githubusercontent.com/ICavlek/SolanaNFT/main/assets/${name.toLowerCase()}.json`;
+        this.uri = 'https://gateway.pinata.cloud/ipfs/QmS95oJiaj8raAnLR13SyowhXsrP4eaWP5rCdxzuNS6ikp'
+    }
+}
---
+++ programs/YoursNFT/src/lib.rs
@@ -0,0 +1,27 @@
+use anchor_lang::prelude::*;
+
+pub mod mint;
+pub mod sell;
+
+use mint::*;
+use sell::*;
+
+declare_id!("AofVbSrzmpWsg4vym4Gq3uCzKqtRjZLTMo8FTvycR8MZ");
+
+#[program]
+pub mod yours_nft {
+    use super::*;
+
+    pub fn mint(
+        ctx: Context<MintNFT>,
+        metadata_title: String,
+        metadata_symbol: String,
+        metadata_uri: String,
+    ) -> Result<()> {
+        mint::mint(ctx, metadata_title, metadata_symbol, metadata_uri)
+    }
+
+    pub fn sell(ctx: Context<SellNFT>, sale_lamports: u64) -> Result<()> {
+        sell::sell(ctx, sale_lamports)
+    }
+}
